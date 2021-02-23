e49874a8b55cd78ecdf4ade4c3de73ac46e84f14
Luis del Moral Martínez <ldmoral1987@gmail.com>
2021-02-23 12:01:55 +0100

    Se añade avatar al heroe y se modifica la estructura para contemplar el campo avatar

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', avatar: '1' },
+  { id: 12, name: 'Narco', avatar: '1'  },
+  { id: 13, name: 'Bombasto', avatar: '1'  },
+  { id: 14, name: 'Celeritas', avatar: '1'  },
+  { id: 15, name: 'Magneta', avatar: '1'  },
+  { id: 16, name: 'RubberMan', avatar: '1'  },
+  { id: 17, name: 'Dynama', avatar: '1'  },
+  { id: 18, name: 'Dr IQ', avatar: '1'  },
+  { id: 19, name: 'Magma', avatar: '1'  },
+  { id: 20, name: 'Tornado', avatar: '1' }
 ];
